9ba4d771597b25e5176bff895ee55b01bb489f86
khanhzoball <khanhhnguyen@g.ucla.edu>
2022-04-15 06:02:11 -0700

    added hello route

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__, static_folder='build/', static_url_path='/')
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/hello')
+def hello():
+    return "hello world"
 
 if __name__ == "__main__":
     app.run(debug=True)
