a58baa2aaa20569b6146cb1f821735a1325fe8f2
khanhzoball <khanhhnguyen@g.ucla.edu>
2022-04-15 19:48:51 -0700

    added hero banner

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
+import banner1 from "./images/banner1.jpg"
 
 function Home () {
     return (
         <div>
-            hi
+            <div className="hero-container">
+                <img src={banner1} className="hero-image"/>
+            </div>
+            <div>
+                <h1>Welcome to DramaNext!!!</h1>
+            </div>
         </div>
     )
 }
