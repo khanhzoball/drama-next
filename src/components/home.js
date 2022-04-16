ca5e5dc899849d5a9292f7ee7daab23b47e7e8d8
khanhzoball <khanhhnguyen@g.ucla.edu>
2022-04-16 03:03:17 -0700

    added search bar

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import banner1 from "./images/banner1.jpg"
+import SearchIcon from '@mui/icons-material/Search';
 
 function Home () {
     return (
@@ -7,8 +8,19 @@ function Home () {
             <div className="hero-container">
                 <img src={banner1} className="hero-image"/>
             </div>
-            <div>
-                <h1>Welcome to DramaNext!!!</h1>
+            <div className="slogan">
+                Welcome to DramaNext!!!
+            </div>
+            <div className="search-description">
+                <span style={{color: "rgb(55, 53, 47)"}}>&#9654;</span> CHOOSE A KDRAMA YOU LIKE AND WE'LL RECOMMEND SOME MORE SIMILAR
+            </div>
+            <div className="search-box">
+                <div className="search-bar">
+                    <input type="text" className="search-input" placeholder="what have you watched recently?"/>
+                    <div className="search-icon">
+                        <SearchIcon />
+                    </div>
+                </div>
             </div>
         </div>
     )
